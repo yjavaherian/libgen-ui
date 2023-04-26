a1f7889a1ed99f1a4fadf6fc4fbe12c2ca1db788
Yousef <yousefjavaherian@gmail.com>
2023-04-26 14:02:51 +0330

    handling different scenarios in getting books from a query

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,20 @@
 import BookList from "@/components/BookList";
-import { getBooks, getIDS } from "@/utils";
+import { getBooks, getIDS, isWhitespace } from "@/utils";
 
 export default async function SearchResults({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
-  const books = await getBooks(await getIDS(searchParams["q"] ?? ""));
-  return <BookList books={books}></BookList>;
+  const query = searchParams["q"];
+  if (query == undefined || isWhitespace(query))
+    throw Error("Please provide a valid search query");
+  try {
+    const books = await getBooks(await getIDS(query));
+    return <BookList books={books}></BookList>;
+  } catch (error) {
+    throw Error(
+      "An Error occurred while communicating with libgen.is make sure you have access to this site."
+    );
+  }
 }
--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -3,6 +3,14 @@ import { IconCalendarEvent, IconNews } from "@tabler/icons-react";
 import BookItem from "./BookItem";
 
 export default function BookList({ books }: { books: Book[] }) {
+  if (books.length == 0) {
+    return (
+      <div className="font-semibold ">
+        <span className="text-4xl">😔</span> No results were found for your
+        query...
+      </div>
+    );
+  }
   return (
     <ol className="flex flex-col gap-2 w-3/4 justify-stretch">
       {books.map((book) => (
