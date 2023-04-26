fb4e11eb28ec1f57a84f3099a7eaa616472512a9
Yousef <yousefjavaherian@gmail.com>
2023-04-26 12:56:14 +0330

    styling SearchBar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import IconEnter from "./IconEnter";
+import { IconArrowBack } from "@tabler/icons-react";
+
+export default function SearchBar() {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+  const onSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+    router.push(`/search?q=${encodeURI(query)}`);
+  };
+  return (
+    <form className="w-1/2 relative group" onSubmit={onSearch}>
+      <div className="absolute -inset-0.5  bg-pink-300 rounded-xl blur-sm opacity-0 group-focus-within:opacity-50 group-focus-within:duration-200 transition duration-1000 animate-tilt"></div>
+      <div className="relative w-full rounded-xl bg-stone-800 px-4 py-2 flex flex-row items-center">
+        <input
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          placeholder="Book name..."
+          className="grow bg-stone-800 focus:outline-none"
+        ></input>
+        <IconArrowBack size={22} />
+      </div>
+    </form>
+  );
+}
