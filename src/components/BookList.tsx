eeefe77f31be47e228a81d204a69b68c0570952e
Yousef <yousefjavaherian@gmail.com>
2023-04-28 02:09:37 +0330

    adding sortbox to booklist. also doing the actual sorting with lodash

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,8 +1,14 @@
+"use client";
+import * as _ from "lodash";
+import { useAtomValue } from "jotai";
 import { Book } from "@/utils";
-import { IconCalendarEvent, IconNews } from "@tabler/icons-react";
+import { sortListAtom } from "@/store";
 import BookItem from "./BookItem";
+import SortBox from "./SortBox";
+import { IconHash } from "@tabler/icons-react";
 
 export default function BookList({ books }: { books: Book[] }) {
+  const sortList = useAtomValue(sortListAtom);
   if (books.length == 0) {
     return (
       <div className="font-semibold ">
@@ -11,11 +17,28 @@ export default function BookList({ books }: { books: Book[] }) {
       </div>
     );
   }
+  const sortedBooks = _.orderBy(
+    books,
+    sortList.map((s) => s.field),
+    sortList.map((s) => s.order)
+  );
   return (
-    <ol className="flex flex-col gap-2 w-3/4 justify-stretch">
-      {books.map((book) => (
-        <BookItem book={book} key={book.id} />
-      ))}
-    </ol>
+    <>
+      <div className="w-1/2 ">
+        <SortBox />
+      </div>
+      <div className=" w-2/3 ">
+        <span className="flex flex-row items-center ml-1 mb-2 ">
+          Books found
+          <IconHash size={16} className="ml-1" />
+          {books.length}
+        </span>
+        <ol className="flex flex-col gap-2 justify-stretch">
+          {sortedBooks.map((book) => (
+            <BookItem book={book} key={book.id} />
+          ))}
+        </ol>
+      </div>
+    </>
   );
 }
